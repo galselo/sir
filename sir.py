7172c3c1fa1cab32ba6ece3e498dd0f337bc1943
tgrassi <tgrassi@nbi.dk>
2020-03-10 09:59:17 +0100

    9 march data

--- sir.py
+++ sir.py
@@ -1,7 +1,10 @@
 # https://scipython.com/book/chapter-8-scipy/additional-examples/the-sir-epidemic-model/
+#https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
 
 # Total population, N.
 N = int(6e7)
@@ -18,7 +21,7 @@ gamma = 1. / 14. # recovery, 1/days
 beta = rnot * gamma  # beta factor
 
 # max days
-xmax = 3e2
+xmax = 125.
 
 # A grid of time points (in days)
 t = np.linspace(0, xmax, 1000)
@@ -43,15 +46,19 @@ S, I, R = ret.T
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111)
-plot = ax.loglog
+plot = ax.semilogy
 plot(t, S, lw=2, label='contagiabili')
 plot(t, I, lw=2, label='infetti')
 plot(t, R, lw=2, label='immuni')
 
+plot(t, I / 10., lw=2, label='TI (10% infetti)')
+plt.hlines(5e3, 0, max(t))
+
 # plot data
 xdata = np.arange(len(data[5])) + 32
 ydata = data[5]
-plot(xdata, ydata, lw=0, marker="o")
+plot(xdata, ydata, lw=0, marker="o", label="infetti (data)")
+plot(xdata, data[2], lw=0, marker="x", label="TI (data)")
 
 ax.set_xlabel('giorni')
 ax.set_ylabel('individui')
@@ -60,11 +67,10 @@ ax.set_ylabel('individui')
 ax.set_xlim(xmin=20)
 ax.set_ylim(ymin=200)
 
-# range zoom data
-#ax.set_xlim(min(xdata)*0.9, max(xdata)*1.1)
-#ax.set_ylim(min(ydata)*0.9, max(ydata)*1.1)
+ax.xaxis.set_minor_locator(MultipleLocator(1))
+ax.xaxis.set_major_locator(MultipleLocator(10))
 
-legend = ax.legend(loc="best")
+legend = ax.legend(loc="best", ncol=2)
 
 #plt.show()
 plt.savefig("sir.png")
